3fe16f0ef5b9f5d79c113098fca70e9abc4d4541
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-17 16:36:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
   }
   //find the index of the item
   var cart = getCart();
-
+  var indexItem = cart.findindex(function(item),);
   
   // removes the specified item from the cart
 }
