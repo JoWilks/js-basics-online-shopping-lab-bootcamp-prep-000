7fa34474baae106bf0d5f3a80b47cbb858203c2b
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-17 17:11:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ function removeFromCart(item) {
     var cart = getCart();
   var removeIndex = cart.findIndex(x => x.itemName== item);
   //check if item exists in cart
-  if (removeIndex = -1) {
+  if (removeIndex === -1) {
     return "That item is not in your cart."
   }
   else {
