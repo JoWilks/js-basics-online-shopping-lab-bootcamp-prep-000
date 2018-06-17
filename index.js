67ff38563ce562ea22fdf8c73419d37efdf44ac4
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-17 15:57:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function viewCart() {
   var objlast = cart[lastno];
   var lastitem = objlast["itemName"];
   var lastprice = objlast["itemPrice"];
-  var allItemsMsg = allItems.join(', ') //combine all items in allItems array into 1 string.
+  var allItemsMsg = allItems.join(' , ') //combine all items in allItems array into 1 string.
   return `In your cart, you have ${allItems}, and ${lastitem} at $${lastprice}.` //return message about what is in cart.
   }
 }
