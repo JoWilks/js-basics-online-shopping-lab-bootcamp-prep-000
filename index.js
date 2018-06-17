af9fa7b5fcdcc812dc884e770f12f8aaa879a5d0
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-17 14:49:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
- var Price = Math.floor((Math.random() * 100) + 1)  //rand gen no between 1-100
+ var Price = Math.floor((Math.random() * 100) + 1);  //rand gen no between 1-100
  var itemObject = {itemName: item, itemPrice: Price};   //create an object with the item name and item price
  cart.push(itemObject);   //push item object into cart array
- return `${item} has been added to your cart.`  //return message saying "'item' has been added to cart"
+ return `${item} has been added to your cart.`;  //return message saying "'item' has been added to cart"
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length ===0) {   
+    return "Your shopping cart is empty";     //if shopping cart empty return "You shopping cart is empty."
+  }
+  else 
 }
 
 function total() {
