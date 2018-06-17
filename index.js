e0d16a8f62027095ad01c69f73df414123932210
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-17 17:13:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,8 +78,11 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
 
 if (cardNumber ==="") {
-  return ""
+  return "Sorry, we don\'t have a credit card on file for you."
 }
-
+else {
+  
+}
+}
 
 }
