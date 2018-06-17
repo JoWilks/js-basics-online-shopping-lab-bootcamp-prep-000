c042438fed9af614982f0785a1d9bff402547500
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-17 17:22:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,12 +76,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-
+var sum = total()
 if (cardNumber ==="") {
   return "Sorry, we don\'t have a credit card on file for you."
 }
 else {
-  var sum = total()
   `Your total cost is ${total}, which will be charged to the card ${cardNumber}.` 
   //empty the cart
   cart = [];
