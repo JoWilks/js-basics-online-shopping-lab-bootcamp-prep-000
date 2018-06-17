f39f52dc9659ce17e37d3c7f8a125be5be2a40a1
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-17 17:29:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,8 +81,10 @@ if (cardNumber === undefined) {
   return "Sorry, we don\'t have a credit card on file for you.";
 }
 else {
+  ] cart = [];
   return `Your total cost is $${sum}, which will be charged to the card ${cardNumber}.`;
   //empty the cart
+  
 }
 }
 
